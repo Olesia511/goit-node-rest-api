6bdc43338035d46671c04c22335c92789238877f
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-01-21 03:34:54 +0200

    wrapper

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,83 +1,45 @@
 const contactsService = require("../services/contactsServices.js");
 const HttpError = require("../helpers/HttpError.js");
+const controllersWrapper = require("../helpers/controllersWrapper.js");
 
 const getAllContacts = async (req, res, next) => {
-  try {
-    const allContacts = await contactsService.listContacts();
-    res.json(allContacts);
-  } catch (error) {
-    next(error);
-  }
+  const allContacts = await contactsService.listContacts();
+  res.json(allContacts);
 };
 
 const getContactById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const searchContact = await contactsService.getContactById(id);
-    if (!searchContact) throw HttpError(404);
+  const { id } = req.params;
+  const searchContact = await contactsService.getContactById(id);
+  if (!searchContact) throw HttpError(404);
 
-    res.json(searchContact);
-  } catch (error) {
-    next(error);
-  }
+  res.json(searchContact);
 };
 
 const deleteContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const deleteContact = await contactsService.removeContact(id);
-    if (!deleteContact) throw HttpError(404);
+  const { id } = req.params;
+  const deleteContact = await contactsService.removeContact(id);
+  if (!deleteContact) throw HttpError(404);
 
-    res.json(deleteContact);
-  } catch (error) {
-    next(error);
-  }
+  res.json(deleteContact);
 };
 
 const createContact = async (req, res, next) => {
-  try {
-    const addedContact = await contactsService.addContact(req.body);
-
-    res.status(201).json(addedContact);
-  } catch (error) {
-    next(error);
-  }
+  const addedContact = await contactsService.addContact(req.body);
+  res.status(201).json(addedContact);
 };
 
 const updateContact = async (req, res, next) => {
-  try {
-    const keysArr = ["name", "email", "phone"];
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const { length } = Object.keys(req.body);
-    const invalidKey = Object.keys(req.body).some(
-      (key) => !keysArr.includes(key)
-    );
-
-    if (length < 1) {
-      throw HttpError(400, "Body must have at least one field");
-    }
-
-    if (invalidKey) {
-      throw HttpError(
-        400,
-        "Body must have only 'name', 'email' or 'phone' fields"
-      );
-    }
-
-    const contact = await contactsService.updateContact(id, req.body);
-    if (!contact) throw HttpError(404);
-
-    res.json(contact);
-  } catch (error) {
-    next(error);
-  }
+  const contact = await contactsService.updateContact(id, req.body);
+  if (!contact) throw HttpError(404);
+  res.json(contact);
 };
 
 module.exports = {
-  getAllContacts,
-  getContactById,
-  deleteContact,
-  createContact,
-  updateContact,
+  getAllContacts: controllersWrapper(getAllContacts),
+  getContactById: controllersWrapper(getContactById),
+  deleteContact: controllersWrapper(deleteContact),
+  createContact: controllersWrapper(createContact),
+  updateContact: controllersWrapper(updateContact),
 };
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,49 +1,114 @@
 const Joi = require("joi");
 
+function trimValidate(string, msg, key) {
+  const trimmedStr = string.trim();
+  if (trimmedStr !== string) {
+    return msg.error(`string.custom.${key}`);
+  }
+  return trimmedStr;
+}
+
+const validateMessages = {
+  "object.unknown": "Only 'name', 'email', and 'phone' keys are allowed",
+
+  "object.min": "Body must have at least one field",
+
+  "object.max":
+    "No more than three fields 'name', 'email', and 'phone' are allowed",
+
+  "string.custom.name":
+    "Name may not contain any spaces at the beginning or end.",
+
+  "string.custom.email":
+    "Email may not contain any spaces at the beginning or end.",
+
+  "string.custom.phone":
+    "Phone may not contain any spaces at the beginning or end.",
+
+  "string.empty": "Field {#label} is not allowed to be empty.",
+
+  "any.required": "Field {#label} is required.",
+};
+
+function stringPatternBaseMsg(keyObj) {
+  switch (keyObj) {
+    case "name":
+      return "The name must consist only of letters of the English alphabet and have from 2 to 30 characters.";
+    case "email":
+      return "Email must contain @ and end with .com or .net. Example: 'dui.Fusce.diam@Donec.com'";
+    case "phone":
+      return "Invalid phone number format. It should be like (111) 222-4444";
+
+    default:
+      return null;
+  }
+}
+
+// ==============    createContactSchema    =========
+
 const createContactSchema = Joi.object({
   name: Joi.string()
-    .pattern(/^[a-zA-Z ]+$/)
-    .message({ "string.pattern.base": "The name must contain only letters" })
-    .min(2)
-    .max(30)
-    .required(),
+
+    .custom((str, msg) => trimValidate(str, msg, "name"))
+    .pattern(new RegExp("^[a-zA-Z]{2}[a-zA-Z ]*$"))
+    .required()
+    .messages({
+      "string.pattern.base": stringPatternBaseMsg("name"),
+    }),
+
   email: Joi.string()
+    .custom((str, msg) => trimValidate(str, msg, "email"))
     .email({
       minDomainSegments: 2,
       tlds: { allow: ["com", "net"] },
     })
-    .message(
-      "Email must contain @ and end with .com or .net. Example: 'dui.Fusce.diam@Donec.com'"
-    )
-    .required(),
+    .required()
+    .messages({
+      "string.email": stringPatternBaseMsg("email"),
+    }),
+
   phone: Joi.string()
-    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-    .message({
-      "string.pattern.base":
-        "The phone number must be of this type (111) 222-3333",
-    })
-    .required(),
-});
+    .custom((str, msg) => trimValidate(str, msg, "phone"))
+    .pattern(new RegExp("^\\(\\d{3}\\) \\d{3}-\\d{4}$"))
+    .required()
+    .messages({
+      "string.pattern.base": stringPatternBaseMsg("phone"),
+    }),
+})
+  .min(3)
+  .max(3)
+  .messages(validateMessages);
+
+// ==============    updateContactSchema    =========
 
 const updateContactSchema = Joi.object({
   name: Joi.string()
-    .pattern(/^[a-zA-Z ]+$/)
-    .message({ "string.pattern.base": "The name must contain only letters" })
-    .min(2)
-    .max(30),
+
+    .custom((str, msg) => trimValidate(str, msg, "name"))
+    .pattern(new RegExp("^[a-zA-Z]{2}[a-zA-Z ]*$"))
+    .messages({
+      "string.pattern.base": stringPatternBaseMsg("name"),
+    }),
+
   email: Joi.string()
+    .custom((str, msg) => trimValidate(str, msg, "email"))
     .email({
       minDomainSegments: 2,
       tlds: { allow: ["com", "net"] },
     })
-    .message(
-      "Email must contain @ and end with .com or .net. Example: 'dui.Fusce.diam@Donec.com'"
-    ),
-  phone: Joi.string()
-    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-    .message({
-      "string.pattern.base":
-        "The phone number must be of this type (111) 222-3333",
+    .messages({
+      "string.email": stringPatternBaseMsg("email"),
     }),
-});
+
+  phone: Joi.string()
+    .custom((str, msg) => trimValidate(str, msg, "phone"))
+    .pattern(new RegExp("^\\(\\d{3}\\) \\d{3}-\\d{4}$"))
+    .messages({
+      "string.pattern.base": stringPatternBaseMsg("phone"),
+    }),
+})
+  .min(1)
+  .max(3)
+  .messages(validateMessages);
+
 module.exports = { createContactSchema, updateContactSchema };
