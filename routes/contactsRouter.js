bd761a1476d15abb02e41b7b34f6690f9b99e478
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-01-26 18:01:51 +0200

    Router

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -25,14 +25,16 @@ contactsRouter.delete("/:id", isIdValid, deleteContact);
 
 contactsRouter.post("/", createContact);
 // contactsRouter.post("/", validateBody(createContactSchema), createContact);
-// contactsRouter.patch(
-//   "/:id/favorite",
-//   isIdValid,
-//   validateBody(favoriteContactSchema),
-//   updateContact
-// );
+
 contactsRouter.put("/:id", isIdValid, updateContact);
 
 // contactsRouter.put("/:id", isIdValid, validateBody(updateContactSchema), updateContact);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isIdValid,
+  validateBody(favoriteContactSchema),
+  updateContact
+);
 
 module.exports = contactsRouter;
